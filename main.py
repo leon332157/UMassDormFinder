df38c417d979197203df3b625704aff165185970
Leon Ge <30728609+leon332157@users.noreply.github.com>
2022-11-12 21:25:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import flask
+from flask import render_template,send_file
+app = flask.Flask(__name__,static_folder = 'static')
+dormList = ["northeast","central","ohill","southwest","honors","sylvan"]
+    
+@app.route('/')
+def index():
+    return send_file("templates/base.html")
+
+@app.route('/index')
+def test():
+    return send_file('templates/index.html')
+
+@app.route('/preview/<name>')
+def preview(name):
+    return send_file(f'templates/{name}.html')
+
+@app.route('/dorm/<name>')
+def dorm(name):
+    if name not in dormList:
+        return ('Not found',404)
+    return render_template('dorm.html',dormName = name)
+#@app.route('/quiz')
+#def quiz():
+#    return render_template("quiz.html")
+app.run('0.0.0.0',0)
