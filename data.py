24b6e7f7898b690417bab7fe80548fe3daac7577
leon332157 <leon332157@gmail.com>
2022-11-13 04:03:18 -0500

    sync

---
+++ data.py
@@ -0,0 +1,29 @@
+dormDict = {"northeast":"Northeast","central":"Central","ohill":"Orchard Hill","southwest":"Southwest","honors":"Honors College","sylvan":"Sylvan"}
+dormList = list(dormDict.keys())
+dormDis = {"northeast":"""First Year Halls: located in Crabtree, Dwight, Hamlin, Leach, Mary Lyon and Knowlton
+Right next to Worcester Dining Commons
+Close proximity to Campus Center, College of Engineering, School of Education, School of Public Health & Health Sciences, Totman Gym
+Volleyball court in the quad""",
+"central":"""First-Year Halls: located in Butterfield, Gorman Hall, Van Meter Hall,Wheeler Hall
+Defined Residential Communities: Spectrum Community for Gay, Lesbian, Bisexual, Transgender students, and allies.
+Near Franklin Dining Commons
+Close proximity to the Studio Arts building, Fine Arts Center, University Health Services, Isenberg School of Management""",
+"ohill":"""First-Year Halls: located in: Dickinson and Webster Halls
+Sweets n’ More in Field Hall
+7-minute walk to Franklin or Worcester Dining Commons
+Overlooking campus, amid rolling hills and apple trees
+Daily exercise""",
+"southwest":"""First-Year Halls: located in Melville, Thoreau, Pierpont , Moore, James, Emerson, Kennedy, and Cance Halls
+Defined Residential Communities": ​Harambee: African Heritage Student Program in Coolidge Hall, El Barrio Community in Washington Hall 
+Near Berkshire and Hampshire Dining Commons
+Close proximity to Hampden Student Center, Southwest Area Government offices, Boyden Gym, Isenberg School of Management""",
+"honors":"""First Year Halls:  Oak and Sycamore
+Honors Advising Center
+Roots Cafe
+Faculty in Residence
+Close proximity to Library, Campus Recreation Center, Student Union, Campus Center, Mullins Center, and Intramural Fields""",
+"sylvan":"""RFYE/Transfer Community in McNamara Hall 
+Fire pits available for student use
+Basketball court
+Snack bar in McNamara
+Wooded trails"""}
--- main.py
+++ main.py
@@ -1,56 +1,77 @@
 import flask
-from flask import render_template,send_file
-app = flask.Flask(__name__,static_folder = 'static')
-dormDict = {"northeast":"Northeast","central":"Central","ohill":"Orchard Hill","southwest":"Southwest","honors":"Honors College","sylvan":"Sylvan"}
-dormList = list(dormDict.keys())
-dormDis = {"northeast":"""First Year Halls: located in Crabtree, Dwight, Hamlin, Leach, Mary Lyon and Knowlton
-Right next to Worcester Dining Commons
-Close proximity to Campus Center, College of Engineering, School of Education, School of Public Health & Health Sciences, Totman Gym
-Volleyball court in the quad""",
-"central":"""First-Year Halls: located in Butterfield, Gorman Hall, Van Meter Hall,Wheeler Hall
-Defined Residential Communities: Spectrum Community for Gay, Lesbian, Bisexual, Transgender students, and allies.
-Near Franklin Dining Commons
-Close proximity to the Studio Arts building, Fine Arts Center, University Health Services, Isenberg School of Management""",
-"ohill":"""First-Year Halls: located in: Dickinson and Webster Halls
-Sweets n’ More in Field Hall
-7-minute walk to Franklin or Worcester Dining Commons
-Overlooking campus, amid rolling hills and apple trees
-Daily exercise""",
-"southwest":"""First-Year Halls: located in Melville, Thoreau, Pierpont , Moore, James, Emerson, Kennedy, and Cance Halls
-Defined Residential Communities": ​Harambee: African Heritage Student Program in Coolidge Hall, El Barrio Community in Washington Hall 
-Near Berkshire and Hampshire Dining Commons
-Close proximity to Hampden Student Center, Southwest Area Government offices, Boyden Gym, Isenberg School of Management""",
-"honors":"""First Year Halls:  Oak and Sycamore
-Honors Advising Center
-Roots Cafe
-Faculty in Residence
-Close proximity to Library, Campus Recreation Center, Student Union, Campus Center, Mullins Center, and Intramural Fields""",
-"sylvan":"""RFYE/Transfer Community in McNamara Hall 
-Fire pits available for student use
-Basketball court
-Snack bar in McNamara
-Wooded trails"""}
-#print(dormDis[])
+from flask import render_template, send_file
+import json
+from data import *
+from werkzeug.utils import secure_filename
+import base64
+from flask_compress import Compress
+
+app = flask.Flask(__name__, static_folder='static')
+#Compress(app)
+app.config['COMPRESS_MIMETYPES'] = ['text/html', 'text/css', 'text/xml',
+                                    'application/json', 'application/javascript', 'image/jpeg', 'image/png', 'image/jpg']
+app.config['COMPRESS_LEVEL'] = 9
+app.config['COMPRESS_MIN_SIZE'] = 10240
+app.config['COMPRESS_BR_LEVEL'] = 11
+app.config["COMPRESS_ALGORITHM"] = ["br", "gzip"]
+GLOBAL_DATA = json.load(open("data.json"))
+
 
 @app.route('/')
 def index():
-    return render_template('index.html',dormList = dormList,dormDict = dormDict)
+    return render_template('index.html', dormList=dormList, dormDict=dormDict)
+
 
 @app.route('/preview/<name>')
 def preview(name):
     return send_file(f'templates/{name}.html')
 
+
 @app.route('/dorm/<name>')
 def dorm(name):
     if name not in dormList:
-        return ('Not found',404)
-    return render_template('dorm.html',dormName = dormDict[name],dormImage = f"/static/imgs/{name}bg.jpeg",dormDis = dormDis[name])
+        return ('Not found', 404)
+
+    return render_template('dorm.html',dormName = name, dormFriendlyName=dormDict[name], dormImage=f"/static/imgs/{name}bg.jpeg", dormDis=dormDis[name], globalData=GLOBAL_DATA)
+
 
 @app.route('/quiz')
 def quiz():
     return send_file("templates/quiz.html")
 
-#@app.route('/quiz')
-#def quiz():
+
+@app.route('/photo/<email>')
+def getPhoto(email):
+    if email not in GLOBAL_DATA:
+        return ('Not found', 404)
+    photob64 = GLOBAL_DATA[email]['photo']
+    if photob64 == "":
+        return ('Not found', 404)
+    return base64.urlsafe_b64decode(photob64), 200, {'Content-Type': GLOBAL_DATA[email]['photoType']}
+
+
+@app.route('/addReview', methods=['POST'])
+def addRating():
+    data = flask.request.form
+    photo = flask.request.files['photo']
+    print(data)
+    if any(str.isdigit(x) for x in data['email']):
+        return flask.redirect(f"{flask.request.referrer}?submitted=false")
+    email = data['email'] + '@umass.edu'
+    rating = int(data['rating'])
+    print(photo)
+    photodata = photo.stream.read()
+    b64data = base64.urlsafe_b64encode(photodata).decode('ascii')
+    GLOBAL_DATA[email] = {"name": data['name'], "rating": rating, "comment": data.get(
+        "comment", None), "hall": data['hall'], "area": data["area"], "photoType": photo.mimetype, "photo": b64data}
+    with open('data.json', 'w') as f:
+        json.dump(GLOBAL_DATA, f)
+    return flask.redirect(f"{flask.request.referrer}?submitted=true")
+
+@app.route('/favicon.ico')
+def favicon():
+    return flask.redirect("https://www.umass.edu/sites/default/files/favicons/favicon-32x32.png")
+# @app.route('/quiz')
+# def quiz():
 #    return render_template("quiz.html")
-app.run('0.0.0.0',8888)
+app.run('0.0.0.0', 8888, debug=True)
