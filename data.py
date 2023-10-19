9c27c8059a3e3df66d9788b7d980b0d0202062e0
Jovy Zhou <zjov@dhcp-10-103-239-67.wireless.northwestern.private>
2023-10-19 15:33:18 -0500

    Get repo

--- data.py
+++ data.py
@@ -1,3 +1,5 @@
+# Database
+
 dormDict = {"northeast":"Northeast","central":"Central","ohill":"Orchard Hill","southwest":"Southwest","honors":"Honors College","sylvan":"Sylvan"}
 dormList = list(dormDict.keys())
 dormDis = {"northeast":"""First Year Halls: located in Crabtree, Dwight, Hamlin, Leach, Mary Lyon and Knowlton
